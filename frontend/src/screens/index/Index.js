d539e05716d27eaabfd2e5e56c53ae68d4fe51e6
carrot <vip.kfkf2013@bk.ru>
2023-03-19 20:31:06 +0300

    fix styles

--- frontend/src/screens/index/Index.js
+++ frontend/src/screens/index/Index.js
@@ -4,15 +4,28 @@ import PageElement from "./element/PageElement";
 
 function Index() {
 
-    const [pages, setPage] = useState([])
+    const [pages, setPage] = useState(null)
+
+    function getPageCount() {
+        return pages.length > 0 ? pages.length : 0;
+    }
 
     return (
         <div>
             <h1>Поиск страниц</h1>
             <InputForm setPage={setPage}/>
-            <div className='topic'>
-                По вашему запросу найдено {pages.length} результатов:
-                {pages.map(page => <PageElement key={page.fileName} path={page.fileName} tfIdf = {page.tfIdf}/>)}
+            <div className='formContainer'>
+                <div className='findPages'>
+                    {pages != null &&
+                        <p> По вашему запросу найдено {getPageCount()} страниц: </p>
+                    }
+                </div>
+            </div>
+            <div className='formContainer'>
+                <div className='findPages'>
+                    {pages != null && pages.length > 0 &&
+                        pages.map(page => <PageElement key={page.fileName} path={page.fileName} tfIdf={page.tfIdf}/>)}
+                </div>
             </div>
         </div>)
 }
--- frontend/src/screens/index/input/InputForm.js
+++ frontend/src/screens/index/input/InputForm.js
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import styles from "./InputForm.module.scss";
 
 function InputForm({setPage}) {
 
@@ -11,16 +12,20 @@ function InputForm({setPage}) {
         e.preventDefault()
         fetch('http://localhost:8080/search/' + data.request)
             .then((response) => response.json())
-            .then(respData => setPage(respData))
+            .then(respData => setPage(respData == null ? [] : respData))
         setData(initData)
     }
 
-    return (<form>
-        <input placeholder='Введите запрос'
-               onChange={e => setData(prev => ({...prev, request: e.target.value}))}
-               value={data.request}/>
-        <button className='button' onClick={(e) => find(e)}>Найти</button>
-    </form>)
+    return (
+        <div className='formContainer'>
+            <form className={styles.form}>
+                <input placeholder='Введите запрос' className={styles.input}
+                       onChange={e => setData(prev => ({...prev, request: e.target.value}))}
+                       value={data.request}/>
+                <button className={styles.button} onClick={(e) => find(e)}>Найти</button>
+            </form>
+        </div>
+    )
 }
 
 export default InputForm;
